9aec9be04e5ad671ddacf7cfc0586fcdd48d4765
André Tebart <tebart@modell-aachen.de>
2018-10-02 11:46:01 +0200

    Adds bubble to waving animation.

---
+++ src/components/BubbleFrames.ts
@@ -0,0 +1,5 @@
+/* tslint:disable:no-var-requires */
+export default [
+    require('../assets/sharkle/sharkle_bubble0.png'),
+    require('../assets/sharkle/sharkle_bubble1.png'),
+  ];
--- src/components/Sharkle.vue
+++ src/components/Sharkle.vue
@@ -1,6 +1,7 @@
 <template>
-<div @click="onClick">
-  <img v-bind:src="sharkleFrame" alt="Hey!">
+<div class="sharkle-container">
+  <img v-show="isWaving" :src="bubbleFrame" class="bubble">
+  <img @click="onClick" :src="sharkleFrame" class="sharkle">
 </div>
 </template>
 
@@ -9,12 +10,16 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 import { FrameAnimation } from './FrameAnimation';
 import IdleAnimationFrames from './IdleFrames';
 import WaveAnimationFrames from './WaveFrames';
+import BubbleFrames from './BubbleFrames';
 
 @Component
 export default class Sharkle extends Vue {
   private idleAnimation!: FrameAnimation;
   private waveAnimation!: FrameAnimation;
+  private bubbleAnimation!: FrameAnimation;
   private sharkleFrame: string = '';
+  private bubbleFrame: string = '';
+  private isWaving: boolean = false;
 
   public created() {
     this.idleAnimation = new FrameAnimation(IdleAnimationFrames, 100, (frame: string) => {
@@ -25,15 +30,29 @@ export default class Sharkle extends Vue {
       this.sharkleFrame = frame;
     });
 
+    this.bubbleAnimation = new FrameAnimation(BubbleFrames, 100, (frame: string) => {
+      this.bubbleFrame = frame;
+    });
+
     this.idleAnimation.start();
   }
 
   public onClick() {
+    this.isWaving = true;
     this.idleAnimation.stop();
     this.waveAnimation.start();
+    this.bubbleAnimation.start();
   }
 }
 </script>
 
 <style scoped lang="scss">
+.sharkle-container {
+  width: 320px;
+  .bubble {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+  }
+}
 </style>
