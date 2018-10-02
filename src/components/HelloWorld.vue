b63a50b1e1852600f15d14d87db5d3dc51301967
André Tebart <tebart@modell-aachen.de>
2018-10-02 10:05:15 +0200

    Adds sharkle idle animation.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,39 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div>
+  <img v-bind:src="animationSource" alt="">
+</div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
+const IDLE_ANIMATION_FRAMES = [
+  'sharkle_idle0.png',
+  'sharkle_idle1.png',
+  'sharkle_idle2.png',
+  'sharkle_idle3.png',
+  'sharkle_idle4.png',
+  'sharkle_idle5.png',
+  'sharkle_idle6.png',
+  'sharkle_idle7.png',
+];
+
+const IDLE_ANIMATION_FRAME_TIME = 100;
+
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+  private currentIdleAnimationFrame: number = 0;
+
+  public mounted() {
+    setInterval(() => {
+      this.currentIdleAnimationFrame = (this.currentIdleAnimationFrame + 1) % IDLE_ANIMATION_FRAMES.length;
+    }, IDLE_ANIMATION_FRAME_TIME);
+  }
+
+  get animationSource() {
+    return require(`../assets/sharkle/${IDLE_ANIMATION_FRAMES[this.currentIdleAnimationFrame]}`);
+  }
 }
 </script>
 
