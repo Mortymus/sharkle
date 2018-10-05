472e1e0d3882e1148b17a6a33faaf20384b6abc2
André Tebart <tebart@modell-aachen.de>
2018-10-05 14:52:36 +0200

    Adds deployment script for gh-pages.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    baseUrl: process.env.NODE_ENV === 'production'
+        ? '/sharkle/'
+        : '/',
     chainWebpack: config => {
         config.module
             .rule('images')
@@ -6,7 +9,7 @@ module.exports = {
             .loader('url-loader')
             .tap(options => Object.assign(options, { limit: 1024000 }));
 
-            config.module
+        config.module
             .rule('media')
             .use('url-loader')
             .loader('url-loader')
